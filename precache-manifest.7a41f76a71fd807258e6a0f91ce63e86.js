112a31235410b1d9fa033aebccb3985598a65a9b
med <mouslfg65@gmail.com>
2021-12-24 17:42:44 -0500

    Updates

---
+++ precache-manifest.7a41f76a71fd807258e6a0f91ce63e86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bdb554d3c32b13a42a190fe56f129c",
+    "url": "/sport/index.html"
+  },
+  {
+    "revision": "804a45fc01b6001cd680",
+    "url": "/sport/static/css/2.33195961.chunk.css"
+  },
+  {
+    "revision": "fc19240acef8dd035307",
+    "url": "/sport/static/css/main.fc4ea939.chunk.css"
+  },
+  {
+    "revision": "804a45fc01b6001cd680",
+    "url": "/sport/static/js/2.4c5379eb.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/sport/static/js/2.4c5379eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc19240acef8dd035307",
+    "url": "/sport/static/js/main.783ae6f8.chunk.js"
+  },
+  {
+    "revision": "288518681a680561f4b9",
+    "url": "/sport/static/js/runtime-main.a7901341.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/sport/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/sport/static/media/Questrial-Regular.37c8a1aa.ttf"
+  },
+  {
+    "revision": "8593e511cdb7d8d64522b0c1073f6096",
+    "url": "/sport/static/media/logos2.8593e511.png"
+  }
+]);
